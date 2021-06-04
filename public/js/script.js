c1b72a471b61fb59d5f3452c3b2985ef7ba8f45f
devart64 <stephendupre64@gmail.com>
2021-06-04 13:58:40 +0200

    mise en place communication entre deux utilisateurs

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,14 @@
 const VideoContainer = document.getElementById('video-container');
 const myVideo = document.getElementById('element-video');
+myVideo.muted = true;
+const socket = io("/");
+const myPeer = new Peer(undefined, {
+    host: "peerjs-server.herokuapp.com",
+    secure: true,
+    port: 443,
+})
 
+const peers = {};
 
 
 navigator.mediaDevices.getUserMedia({
@@ -8,10 +16,46 @@ navigator.mediaDevices.getUserMedia({
     audio: false
 }).then((stream) => {
     console.log(stream)
-    myVideo.srcObject = stream
-    myVideo.addEventListener('loadmetadata', (e) => {
-        myVideo.muted = true,
-        myVideo.play()
+
+    addVisedoStream(myVideo, stream);
+
+    myPeer.on('call', call => {
+        call.answer(stream);
+        const video = document.getElementById('element-video-exterieur');
+        call.on('stream', (userVideoStream) => {
+            addVisedoStream(video, userVideoStream);
+        })
+    })
+
+    socket.on('user-connect', (userId) => {
+        console.log('user ID: ' + userId)
+        connectToNewUser(userId, stream);
+    })
+
+}); 
+
+myPeer.on('open', id => {
+    socket.emit("join-room", ROOM_ID, id); 
+})
+
+
+
+
+
+
+connectToNewUser = (userId, stream) => {
+    const call = myPeer.call(userId, stream);
+    const video = document.getElementById('element-video');
+    call.on('stream', (userVideoStream) => {
+        addVisedoStream(video, userVideoStream);
+    })
+}
+
+
+addVisedoStream = (video, stream) => {
+    video.srcObject = stream
+    video.addEventListener('loadmetadata', (e) => {
+        video.play()
     });
-    VideoContainer.append(myVideo);
-});
+    VideoContainer.append(video);
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.set('view  engine', 'ejs')
 app.use(express.static("public"));
@@ -11,6 +12,16 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
     res.render("room.ejs", {roomId: req.params.room});
+})
+
+
+io.on("connection", (socket) => {
+    console.log('user connect => ' + socket.id)
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId); // on join le user à la room
+        console.log('room id = '+ roomId)
+        socket.to(roomId).emit("user-connect", userId); // une fois que l'utilisateur rejoint la room on notifie tous les utilisateurs de la room
+    })
 })
 
 server.listen(3000);
